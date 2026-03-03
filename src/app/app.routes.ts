6677aea2f9c1573562996d123f23636c2abdfa6d
quyn03z <quyenvn03@gmail.com>
2026-03-03 14:42:20 +0700

    update front-end admin and profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,13 +7,16 @@ import { ForgotPasswordComponent } from './modules/account/forgot-password/forgo
 import { ResetPasswordComponent } from './modules/account/reset-password/reset-password.component';
 import { CourseComponent } from './pages/course/course.component';
 import { CourseDetailsComponent } from './pages/course-details/course-details.component';
+import { AdminComponent } from './modules/admin/admin.component';
+import { ProfileComponent } from './modules/profile/profile.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     // Trang login - KHÔNG có header/footer
     { path: 'login', component: LoginComponent },
     { path: 'forgot-password', component: ForgotPasswordComponent },
     { path: 'reset-password', component: ResetPasswordComponent },
-
+    { path: 'admin', component: AdminComponent },
     // Các trang CÓ header/footer (bọc trong MainLayout)
     {
         path: '',
@@ -22,6 +25,7 @@ export const routes: Routes = [
             { path: 'home', component: HomeComponent },
             { path: 'course', component: CourseComponent },
             { path: 'course-details/:courseId', component: CourseDetailsComponent },
+            { path: 'profile', component: ProfileComponent },
             // Thêm các route khác ở đây, ví dụ:
             // { path: 'courses', component: CoursesComponent, canActivate: [authGuard] },
         ]
