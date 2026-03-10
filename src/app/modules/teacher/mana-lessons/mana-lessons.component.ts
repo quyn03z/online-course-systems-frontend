fcc672172d95de6c7a647a0010e8321a8ac39c47
quyn03z <quyenvn03@gmail.com>
2026-03-10 17:04:55 +0700

    add new branch

---
+++ src/app/modules/teacher/mana-lessons/mana-lessons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mana-lessons',
+  standalone: true,
+  imports: [],
+  templateUrl: './mana-lessons.component.html',
+  styleUrl: './mana-lessons.component.scss'
+})
+export class ManaLessonsComponent {
+
+}
