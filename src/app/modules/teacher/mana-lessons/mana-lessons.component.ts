114849897f1b73d95785cd71b0c227f43450391c
quyn03z <quyenvn03@gmail.com>
2026-03-11 13:58:26 +0700

    add mana-lessons

--- src/app/modules/teacher/mana-lessons/mana-lessons.component.ts
+++ src/app/modules/teacher/mana-lessons/mana-lessons.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mana-lessons',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './mana-lessons.component.html',
   styleUrl: './mana-lessons.component.scss'
 })
---
+++ src/app/modules/teacher/mana-quizz/mana-quizz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mana-quizz',
+  standalone: true,
+  imports: [],
+  templateUrl: './mana-quizz.component.html',
+  styleUrl: './mana-quizz.component.scss'
+})
+export class ManaQuizzComponent {
+
+}
